fe8fb0b106bce4d0d89cf59511447d8e0c827b63
Yonatan Rojas S <yonatan@bedu.org>
2019-01-13 19:32:06 -0600

    added psycopg2

--- series/series/settings.py
+++ series/series/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'serieslist'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'series.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'series_api',
+        'USER': 'yonatan',
+        'PASSWORD': '*secret00*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
